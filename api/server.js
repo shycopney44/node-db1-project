d75e8b0b20d89fb5895541c5407de5e8485905a7
Shy Copney <shydashacopney@gmail.com>
2025-04-01 10:19:01 -0400

    endpoints wired and working with err handling

--- api/server.js
+++ api/server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: "Not Found"
+    })
+})
+
 module.exports = server;
