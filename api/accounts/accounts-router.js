4eba351fbf23582abce819ebfcd82138153261d0
Shy Copney <shydashacopney@gmail.com>
2025-04-02 11:27:24 -0400

    complete project

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,49 +1,62 @@
 const router = require('express').Router()
 const md = require('./accounts-middleware')
+const Account = require('./accounts-model') 
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+
+router.get('/', async (req, res, next) => {
   try {
-    res.json('get accounts')
+    const accounts = await Account.getAll()
+    res.json(accounts)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:id', md.checkAccountId,  (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-
-  }
+router.get('/:id', md.checkAccountId, async (req, res, 
+  next) => {
+    try {
+      const account = await Account.getById(req.params.id)
+      res.json(account)
+    } catch (err) {
+      next(err)
+    }
 })
 
 router.post(
   '/', 
   md.checkAccountPayload, 
   md.checkAccountNameUnique, 
-  (req, res, next) => {
-  // DO YOUR MAGIC
+  async (req, res, next) => {
   try {
-
+    const newAccount = await Account.create({ 
+      name: req.body.name.trim(),
+      budget: req.body.budget,
+    })
+    res.status(201).json(newAccount)
+  } catch (err) {
+    next(err)
   }
 })
 
 router.put(
   '/:id', 
   md.checkAccountId, 
-  md.checkAccountPayload, 
-  md.checkAccountNameUnique, 
-  (req, res, next) => {
-  // DO YOUR MAGIC
+  md.checkAccountPayload,  
+  async (req, res, next) => {
   try {
-
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
+  } catch (err) {
+    next(err)
   }
 });
 
-router.delete('/:id', md.checkAccountId, (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   try {
-
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
+  } catch (err) {
+    next(err)
   }
 })
 
